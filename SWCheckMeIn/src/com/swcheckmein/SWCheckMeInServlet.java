25b6f3884ac2b96e36f243777b15a8ea13cb4862
cvvick <cvvick@yahoo.com>
2013-06-14 15:53:22 -0700

    added Aiport to servlet add persistence with CachedAirport

--- SWCheckMeIn/src/com/swcheckmein/SWCheckMeInServlet.java
+++ SWCheckMeIn/src/com/swcheckmein/SWCheckMeInServlet.java
@@ -1,11 +1,32 @@
 package com.swcheckmein;
 import java.io.IOException;
+
 import javax.servlet.http.*;
+
+import com.swcheckmein.airport.Airport;
 
 @SuppressWarnings("serial")
 public class SWCheckMeInServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/plain");		
+		Airport airport = Airport.lookup("PDX");
+		resp.getWriter().println("fs = " + airport.getFs());
+		resp.getWriter().println("iata = " + airport.getIata());	
+		resp.getWriter().println("icao = " + airport.getIcao());
+		resp.getWriter().println("faa = " + airport.getFaa());
+		resp.getWriter().println( "name = " + airport.getName());
+		resp.getWriter().println( "street1 = " + airport.getStreet1());
+		resp.getWriter().println( "street2 = " + airport.getStreet2());
+		resp.getWriter().println( "city = " + airport.getCity());
+		resp.getWriter().println( "cityCode = " + airport.getCityCode());
+		resp.getWriter().println( "stateCode = " + airport.getStateCode());
+		resp.getWriter().println( "postalCode = " + airport.getPostalCode());
+		resp.getWriter().println( "countryCode = " + airport.getCountryCode());
+		resp.getWriter().println( "countryName = " + airport.getCountryName());
+		resp.getWriter().println( "regionName = " + airport.getRegionName());
+		resp.getWriter().println( "timeZoneRegionName = " + airport.getTimeZoneRegionName());
+		resp.getWriter().println( "weatherZone = " + airport.getWeatherZone());
+		resp.getWriter().println( "latitude = " + String.valueOf( airport.getLatitude()));
+		resp.getWriter().println( "longitude =" + String.valueOf( airport.getLongitude() ));
 	}
 }
--- SWCheckMeIn/src/com/swcheckmein/airport/Airport.java
+++ SWCheckMeIn/src/com/swcheckmein/airport/Airport.java
@@ -3,9 +3,11 @@ package com.swcheckmein.airport;
 import java.lang.NullPointerException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.List;
 
 import javax.json.Json;
 import javax.json.stream.JsonParser;
@@ -21,7 +23,6 @@ import javax.persistence.EntityManager;
  * It first checks to see if the airport is cached locally in the database. If not, it retrieves the
  * information from the FlightStats service
  */
-@Entity
 public class Airport {
 	/* Web service end point for the Airport REST API */
 	private static final String flightStatsEndpoint = "	https://api.flightstats.com/flex/airports/rest/v1/json/iata/";
@@ -59,16 +60,32 @@ public class Airport {
 		if (!(iataCode instanceof String) && (iataCode.length() ==3))
 			throw new IllegalArgumentException( "Parameter must be an instance of String and be 3 characters long");
 		
-		/* TODO: Check if we've already retrieved the airport before and retrieve from the database */
+		/* Check to see if we've already retrieved this airport from FlightStats */
 		EntityManager em = EMF.get().createEntityManager();
+		
+		List<CachedAirport> result = em.createQuery( 
+                "SELECT e " + 
+                "FROM CachedAirport e "+ 
+                "WHERE e.name = ?1 ",  CachedAirport.class) 
+          .setParameter(1, iataCode) 
+          .getResultList(); 
+		
+		int count = 0; 
+		for (CachedAirport e : result) { 
+			System.out.println(++count + ": " + e.getIata() + ", " + e.getName()); 
+		} 
+		
+		/* TODO: Copy the CachedAirport to a new Airport instance */
 		
 		/* retrieve the data from the FlightStats web service */
 		Airport airport = null;
 		try
 		{
 			URL fsAirportRequest = new URL( flightStatsEndpoint + iataCode + "?appId=" + appId + "&appKey=" +appKey );
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(fsAirportRequest.openStream()));
+						
+			URLConnection connection = fsAirportRequest.openConnection();
+			/* connection.setConnectTimeout(0); */
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 		
 			JsonParser jsonParser = Json.createParser(reader);
 			airport = new Airport();
@@ -165,20 +182,26 @@ public class Airport {
 						event = jsonParser.next(); /* get the value */
 						airport.longitude = Double.parseDouble(jsonParser.getString());
 						break;
-
 					}
 				}
 				event=jsonParser.next();
 			}
 			jsonParser.close();
+			
+			/* Store the airport information retrieved from FlightStats */
+			em.getTransaction().begin();
+			
+			CachedAirport cachedAirport = CachedAirport.valueOf( airport );
+			em.persist(cachedAirport);
+			em.getTransaction().commit();		
 		}
 		catch (MalformedURLException e)
 		{
-			System.out.println("MalformedURLException in Airport.lookup()");
+			System.out.println("MalformedURLException in Airport.lookup(): " + e.getMessage());
 		}
 		catch (IOException e)
 		{
-			System.out.println("IOException in Airport.lookup()");
+			System.out.println("IOException in Airport.lookup(): " + e.getMessage());
 		}
 		finally
 		{
@@ -188,6 +211,205 @@ public class Airport {
 		return airport;
 	}
 
+	public String getFs() {
+		return fs;
+	}
+
+	public String getIata() {
+		return iata;
+	}
+
+	public String getIcao() {
+		return icao;
+	}
+
+	public String getFaa() {
+		return faa;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getStreet1() {
+		return street1;
+	}
+
+	public String getStreet2() {
+		return street2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getCityCode() {
+		return cityCode;
+	}
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public String getTimeZoneRegionName() {
+		return timeZoneRegionName;
+	}
+
+	public String getWeatherZone() {
+		return weatherZone;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+}
+
+/*
+ * Cached airport entity that represents a airport that has been previously retrieved from Flightstats
+ */
+@Entity
+class CachedAirport
+{
+	private String fs; 			/*	The FlightStats code for the airport, globally unique across time.*/
+	@Id
+	private String iata;			/* The IATA code for the airport */
+	private String icao;			/* The ICAO code for the airport */
+	private String faa;			/* The FAA code for the airport */
+	private String name;			/* The name of the airport */
+	private String street1;		/* The street address of the airport, part 1 */
+	private String street2; 		/* Street address of the airport, part 2 */
+	private String city;			/* The city with which the airport is associated */
+	private String cityCode;		/* The city code with which the airport is associated */
+	private String stateCode; 	/* State in which the airport is located */
+	private String postalCode; 	/* The postal code in which the airport resides */
+	private String countryCode; 	/* The code for the country in which the airport is located */
+	private String countryName; 	/* The name of the country in which the Airport is located. */
+	private String regionName; 	/* The name of the region in which the Airport is located */
+	private String timeZoneRegionName; /* The name of the Time Zone region in which the Airport is located */
+	private String weatherZone; 	/* The NOAA weather zone (US only) in which the Airport is located */
+	private double latitude; 		/* The latitude of the airport in decimal degrees.*/
+	private double longitude;		/* The longitude of the airport in decimal degrees.*/
+	
+	public CachedAirport()
+	{  }
+	
+	public static CachedAirport valueOf(Airport airport)
+	{
+		CachedAirport cachedAirport = new CachedAirport();
+		
+		/* defensive copy */
+		cachedAirport.setFs(new String(airport.getFs()));
+		cachedAirport.setIata(new String(airport.getIata()));
+		cachedAirport.setIcao(new String(airport.getIcao()));
+		cachedAirport.setFaa(new String(airport.getFaa()));
+		cachedAirport.setName(new String(airport.getName()));
+		cachedAirport.setStreet1(new String(airport.getStreet1()));
+		cachedAirport.setStreet2(new String(airport.getStreet2()));
+		cachedAirport.setCity(new String(airport.getCity()));
+		cachedAirport.setCityCode(new String(airport.getCityCode()));
+		cachedAirport.setStateCode(new String(airport.getStateCode()));
+		cachedAirport.setPostalCode(new String(airport.getPostalCode()));
+		cachedAirport.setCountryCode(new String(airport.getCountryCode()));
+		cachedAirport.setCountryName(new String(airport.getCountryName()));
+		cachedAirport.setRegionName(new String(airport.getRegionName()));
+		cachedAirport.setTimeZoneRegionName(new String(airport.getTimeZoneRegionName()));
+		cachedAirport.setWeatherZone(new String(airport.getWeatherZone()));
+		cachedAirport.setLatitude(airport.getLatitude());
+		cachedAirport.setLongitude(airport.getLongitude());
+		return cachedAirport;
+	}
+	
+	public void setFs(String fs) {
+		this.fs = fs;
+	}
+
+	public void setIata(String iata) {
+		this.iata = iata;
+	}
+
+	public void setIcao(String icao) {
+		this.icao = icao;
+	}
+
+	public void setFaa(String faa) {
+		this.faa = faa;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setStreet1(String street1) {
+		this.street1 = street1;
+	}
+
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public void setTimeZoneRegionName(String timeZoneRegionName) {
+		this.timeZoneRegionName = timeZoneRegionName;
+	}
+
+	public void setWeatherZone(String weatherZone) {
+		this.weatherZone = weatherZone;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 
 	public String getFs() {
 		return fs;
